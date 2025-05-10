f4bafdbff53d75a048a7234f79532b95666eacee
dldri <diovian.liu@gmail.com>
2025-05-10 14:58:51 +0800

    updated pwsh profile to include vim alias

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -6,3 +6,6 @@ function touch {New-Item "$args" -ItemType File}
 # oh-my-posh
 # using catppuccin-mocha theme
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_mocha.omp.json" | Invoke-Expression
+
+# Set vim alias to run nvim
+Set-Alias -Name vim -Value nvim
